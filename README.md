fd655d62bd2f0ded6cd6104a4f545bdeeceb5330
Joshua Coleman <procoleman77@outlook.com>
2020-07-29 12:12:31 -0400

    add functions version to code" git push "

--- README.md
+++ README.md
@@ -1,9 +1,17 @@
 # Javascript
 This is program created to build a todo list using different JS methods as skills increase.
 
-V1
+V1 Arrays
 1.) It should have a place to store todo's.
 2.) It should have a way to display todo's.
-3.)It should have a way to add new todo's.
-4.)It should have a way to change a todo's.
-5.)It should have a way to delete a todo.
+3.) It should have a way to add new todo's.
+4.) It should have a way to change a todo's.
+5.) It should have a way to delete a todo.
+
+V2 functions
+1.) It should have a place to store todo's.(array)
+2.) It should have a way to display todo's.
+3.) It should have a way to add new todo's.
+4.) It should have a way to change a todo's.
+5.) It should have a way to delete a todo.
+
--- new.js
+++ new.js
@@ -1,14 +1,135 @@
 // v1
-// Array
+// // Array  It should have a place to store todo's.
+// var todos = ["item 1", "item 2", "item 3"];
+
+// // Test
+// console.log(todos);
+
+// // It should have a way to display todo's.
+
+// console.log("My todo's:", todos);
+
+
+// // It should have a way to add new todo's.
+
+// todos.push("item 4", "item 5");
+
+// // It should have a way to change a todo's.
+
+// todos[0] = "item 1 updated";
+
+// // It should have a way to delete a todo.
+// todos.splice(0, 1)
+
+// // ------------------------------------ V2 Functions-------------------------
+// // It should have a place to store todo's.(array)
+// var todos = ['item One', 'item Two', 'item Three', 'item Four'];
+
+// function displayTodos() {
+//     // It should have a way to display todo's.
+// //     console.log('my Todos:', todos);
 
 
 
+// // }
+
+// // displayTodos();
+
+// // // It should have a way to add new todo's.
+// // function addTodo(todo) {
+// //     todos.push(todo);
+// //     displayTodos();
 
 
 
+// // }
+
+// // addTodo('andrew');
+
+// // //  It should have a way to change a todo's.
+
+// // function updateTodo(position, change) {
+
+// //     todos[position] = change;
+// //     displayTodos();
+// // }
+
+// // updateTodo(1, "got it");
+
+// // //  It should have a way to delete a todo.
+
+// // function deleteTodo(position) {
+
+// //     todos.splice(position, 1);
+// //     displayTodos();
 
 
+// // }
+
+// // deleteTodo(1);
+
+// // Repeat exercise
+// // It should have a place to store todo's.(array)
+// var drinks = ['soda', 'juice', 'water', 'pepsi'];
+
+// // It should have a way to display todo's.
+
+// function displayDrinks() {
+
+//     console.log("My Dirnks:", drinks);
 
 
+// }
+
+// displayDrinks();
+
+// // It should have a way to add new todo's.
+
+// function addDrinks(drink) {
+
+//     drinks.push(drink);
+
+//     displayDrinks();
+// }
+
+// addDrinks("Club soda");
+
+// // It should have a way to change a todo's.
+
+// function updateDrinks(position, change) {
+
+//     drinks[position] = change;
+
+//     displayDrinks();
+
+// }
+
+// updateDrinks(0, 'lemon water');
+
+// //  It should have a way to delete a todo.
+
+// function deleteDrinks(position) {
 
 
+//     drinks.splice(position, 1);
+
+//     displayDrinks();
+
+// }
+
+// deleteDrinks(0);
+// ---------------------------Objects v3-------------------
+
+// Example
+
+var myComputer = {
+
+    operatingSystem: 'mac',
+    screenSize: '15inches',
+    purchaseYear: 2020
+
+}
+
+console.log(myComputer);
+
+console.log(myComputer.operatingSystem);
